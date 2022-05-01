d1084d229f246f48ebb68784d12a3b52e740dfe5
Jérémy Serenne <jeremy.serenne@epitech.eu>
2022-05-01 14:02:10 +0000

    {main}: add cmd line through console

--- index.js
+++ index.js
@@ -15,7 +15,20 @@ const twitchChannel = process.env['TWITCH_CHANNEL'];
 
 const cmdAndResList = process.env['LIST_OF_CMDS'].split(';'); // format cmd:res;cmd:res
 
+const cmdLineList = process.env['LIST_OF_CMDS_LINE'].split(';'); // format cmd:r,es;cmd:res  , -> end of line
+
 let cmdDict = {};
+
+let cmdLineDict = {};
+
+function setUpCmdLineDict() {
+  for (var i in cmdLineList) {
+    var cmdAndRes = cmdLineList[i].split(':');
+    cmdLineDict[cmdAndRes[0]] = cmdAndRes[1];
+  }
+}
+
+setUpCmdLineDict();
 
 async function setUpDB() {
   for (var i in cmdAndResList) {
@@ -59,7 +72,7 @@ client.on('message', async (channel, context, message) => {
 
   let res = await handleCmd(argsDict);
   if (res) {
-    client.say(channel, "Prayge i"); // Dodge the 30sec to wait in case of identical message error
+    client.say(channel, "i"); // Dodge the 30sec to wait in case of identical message error
     await delay(5000);
   }
   client.say(channel, res);
@@ -133,3 +146,30 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log(`Server started on ${twitchChannel}`);
 });
+
+
+const readline = require('readline');
+// const fileStream = fs.createReadStream('input.txt');
+
+const rl = readline.createInterface({
+  input: process.stdin, //or fileStream 
+  output: process.stdout
+});
+
+
+const start = async () =>{
+  for await (const line of rl) {
+    if (!cmdLineDict[line]) {
+      console.log(`line=${line}`);
+    } else {
+    var parsedMsg = cmdLineDict[line].split(',');
+       for (var i in parsedMsg) {
+         await delay(1100);
+         client.say(twitchChannel, parsedMsg[i]);
+         // console.log(parsedMsg[i]);
+       }
+    }
+   }
+}
+
+start()
