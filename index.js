eb5694f2449f526db9a85b756cf13d031faa7a6b
Jérémy Serenne <jeremy.serenne@epitech.eu>
2022-05-09 16:37:16 +0000

    {main}: generate stats from a bot

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const moment = require('moment');
+
+const fs = require('fs');
 
 const axios = require('axios');
 
@@ -12,6 +15,8 @@ const tmi = require('tmi.js');
 require('dotenv').config();
 
 const twitchChannel = process.env['TWITCH_CHANNEL'];
+
+const botListeningTo = process.env['TWITCH_BOT_NAME_LISTENER']
 
 const cmdAndResList = process.env['LIST_OF_CMDS'].split(';'); // format cmd:res;cmd:res
 
@@ -68,19 +73,52 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 // Connect the client to the server..
 client.connect();
 
+// client.on('connect', function(connection) {
+//   console.log('WebSocket Client Connected');
+//   daily_message.msg1.start(client)
+// });
+
+function getBotData(botName, filePath, message) {
+  let transaction = '';
+  const amount = message.match(/(\d+)/)[0];
+  const date = moment().format('DD/MM/YYYY HH:mm:ss');
+
+  if (message.includes('-')) {
+    transaction = '-';
+  } else if (message.includes('+')) {
+    transaction = '+';
+  } else {
+    return;
+  }
+  var logStream = fs.createWriteStream(filePath, {flags: 'a'});
+    logStream.write(`${transaction} ${amount} ${date}`);
+    logStream.end('\n');
+}
+
 client.on('message', async (channel, context, message) => {
+
+  if (context.username == botListeningTo) {
+     getBotData(botListeningTo, 'stats.txt', message);
+     console.log(`${botListeningTo} said ${message}.`);
+  }
   if (!message.startsWith('!')) return;
+
 
   const argsDict = {channel: channel.slice(1), context: context, args: message.slice(1).split(' ')};
 
   let res = await handleCmd(argsDict);
   if (res) {
+    await delay(1000);
     client.say(channel, "i"); // Dodge the 30sec to wait in case of identical message error
     console.log(`${res} will be send.`);
-    await delay(3000);
-    console.log(`${res} is sent!`);
+    await delay(1500);
+    console.log(`${res} was sent!`);
   }
   client.say(channel, res);
+
+  if (context.username == process.env['TWITCH_BOT_USERNAME']) {
+     console.log(`You said ${message}.`);
+  }
 });
 
 /**
@@ -152,6 +190,34 @@ app.listen(3000, () => {
   console.log(`Server started on ${twitchChannel}`);
 });
 
+/* Scheduled actions */
+
+// async function dailyCmd() {
+//   var dailyTime = new Date().getTime();
+//   console.log(new Date(dailyTime).toString());
+  
+//   var parsedMsg = cmdLineDict['!daily'].split(',');
+//   for (var i in parsedMsg) {
+//     await delay(2000);
+//     client.say(twitchChannel, parsedMsg[i]);
+//   }
+// }
+
+// function setDailyMsg() {
+//   setInterval(dailyCmd, 86400000);
+//   // dailyCmd();
+// }
+
+// var currentTime = new Date().getTime();
+// var time = 25200000;
+
+
+// setTimeout(setDailyMsg, time-currentTime);
+// console.log(new Date(currentTime).toString());
+
+
+
+/* */
 
 const readline = require('readline');
 // const fileStream = fs.createReadStream('input.txt');
@@ -164,7 +230,7 @@ const rl = readline.createInterface({
 // !cmd:nb
 
 const start = async () =>{
-  for await (const line of rl) {
+  for await (const line of rl) {    
     let cmd = line.split(':')[0];
     let arg = parseInt(line.split(':')[1]);
 
@@ -174,7 +240,7 @@ const start = async () =>{
       } else {
       var parsedMsg = cmdLineDict[cmd].split(',');
          for (var i in parsedMsg) {
-           await delay(1100);
+           await delay(1050);
            client.say(twitchChannel, parsedMsg[i]);
            // console.log(parsedMsg[i]);
          }
@@ -182,4 +248,4 @@ const start = async () =>{
    }
 }
 
-start()
+start();
